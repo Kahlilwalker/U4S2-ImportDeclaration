96e40050703d6880a9c7e6285e70e5e257e5ee34
Kahlilwalker <Kahlilmyson@gmail.com>
2024-12-31 12:15:41 -0500

    todays work

--- src/main/java/partA/activity/Activity01_ExplicitImport.java
+++ src/main/java/partA/activity/Activity01_ExplicitImport.java
@@ -1,4 +1,21 @@
 package partA.activity;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Activity01_ExplicitImport {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int total = 0;
+
+        int count = 3;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.println("Enter integer " + i + ":");
+            total += scan.nextInt();
+        }
+
+        System.out.println("The sum is: " + total);
+        scan.close();
+    }
 }
--- src/main/java/partA/activity/Activity02_NonExplicitImport.java
+++ src/main/java/partA/activity/Activity02_NonExplicitImport.java
@@ -1,4 +1,20 @@
 package partA.activity;
 
 public class Activity02_NonExplicitImport {
+    public static double tax = .05;
+    public  static double tip = .15;
+
+    public static double findTotal(double price){
+        double total = price*(1+tax+tip);
+        System.out.println("$" +total);
+
+
+        return total;
+    }
+    public static void main(String[] args) {
+        System.out.println(findTotal(34));
+    }
+
 }
+
+
